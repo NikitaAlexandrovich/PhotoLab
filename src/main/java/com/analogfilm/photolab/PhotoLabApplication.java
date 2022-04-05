e46a777d2173fb0751374da5c7bc829f09e644d0
Nikita <nikitahodarenok@gmail.com>
2022-04-05 11:47:08 +0300

    Initialization project structure.

---
+++ src/main/java/com/analogfilm/photolab/PhotoLabApplication.java
@@ -0,0 +1,13 @@
+package com.analogfilm.photolab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoLabApplication.class, args);
+    }
+
+}
