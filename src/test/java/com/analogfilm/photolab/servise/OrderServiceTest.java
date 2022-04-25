aab578dc4209ce6160fda75a7be3afeb90d8b4d4
NikitaAlexandrovich <nikitahodarenok@gmail.com>
2022-04-25 19:44:27 +0300

    rebase 2, exampl

--- src/test/java/com/analogfilm/photolab/servise/OrderServiceTest.java
+++ src/test/java/com/analogfilm/photolab/servise/OrderServiceTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+// https://www.youtube.com/watch?v=QrSR1fm9JwA&t=518s
+
 import static org.junit.Assert.assertEquals;
 
 public class OrderServiceTest {
