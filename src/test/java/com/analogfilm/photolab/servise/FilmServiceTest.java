8e19f90350b7945ac035e0c2db4ce2d46d4885a0
NikitaAlexandrovich <nikitahodarenok@gmail.com>
2022-04-25 19:44:27 +0300

    rebase 2, exampl

---
+++ src/test/java/com/analogfilm/photolab/servise/FilmServiceTest.java
@@ -0,0 +1,24 @@
+package com.analogfilm.photolab.servise;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FilmServiceTest {
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void deleteFilm() {
+    }
+
+    @Test
+    void findByName() {
+    }
+
+    @Test
+    void saveFilm() {
+    }
+}
