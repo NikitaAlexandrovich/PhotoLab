6bb37a1f086168fc285563f75f4222879d5d9083
NikitaAlexandrovich <nikitahodarenok@gmail.com>
2022-04-25 20:01:03 +0300

    add scan service test

---
+++ src/test/java/com/analogfilm/photolab/servise/ScanServiceTest.java
@@ -0,0 +1,24 @@
+package com.analogfilm.photolab.servise;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ScanServiceTest {
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void deleteScan() {
+    }
+
+    @Test
+    void findByName() {
+    }
+
+    @Test
+    void saveScan() {
+    }
+}
