ce62ec0248e9dd2477fb3b76ebf46660f27deac8
NikitaAlexandrovich <nikitahodarenok@gmail.com>
2022-04-25 18:38:40 +0300

    1 add Film export test

---
+++ src/test/java/com/analogfilm/photolab/servise/FilmExportTest.java
@@ -0,0 +1,12 @@
+package com.analogfilm.photolab.servise;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FilmExportTest {
+
+    @Test
+    void export() {
+    }
+}
